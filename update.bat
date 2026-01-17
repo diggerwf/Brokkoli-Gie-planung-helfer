8bb2fd36611cf90440928b5681c4069f5baa2afb
diggerwf <80178091+diggerwf@users.noreply.github.com>
2026-01-17 15:01:05 +0100

    Update update.bat

--- update.bat
+++ update.bat
@@ -54,6 +54,7 @@ if exist ".git\" (
         echo 🆕 Selbst-Update erkannt! Starte Sicherheits-Prozess... 📥
         copy /y "%SELF_NAME%" "%TEMP_NAME%" >nul
         start "" "%TEMP_NAME%"
+        taskkill /f /im cmd.exe /fi "WINDOWTITLE eq %CD%*" >nul 2>&1
         exit
     ) else (
         echo ✅ Alles aktuell!
@@ -81,6 +82,7 @@ if exist "!START_FILE!" (
 )
 exit
 ::test
+
 
 
 
